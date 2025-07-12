ff60d22331c4fee44aed00eacdc148c79679c4bd
Israr8957 <ik6141333@gmail.com>
2025-07-12 12:06:37 +0530

    ..

--- src/data/cardData.ts
+++
@@ -1,5 +0,0 @@
-const data = [
-  { title: 'Card 1', description: 'This is the first card.' },
-  { title: 'Card 2', description: 'This is the second card.' },
-  { title: 'Card 3', description: 'This is the third card.' },
-];
