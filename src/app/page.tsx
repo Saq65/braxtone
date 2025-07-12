a9fd54b01ad2b8690d13ad55cd9d0ef597ef22ca
Israr8957 <ik6141333@gmail.com>
2025-07-12 12:42:09 +0530

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h2 className="text-2xl font-bold text-center bg-[#4]">Braxtone</h2>
+    <div className="text-center">
+     <h2 className="">Braxtone</h2>
+      
     </div>
   );
 }
