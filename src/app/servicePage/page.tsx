dc9cafcdbd55f68b7066d8a1f00b22085c9bfdbf
Israr8957 <ik6141333@gmail.com>
2025-07-14 14:50:41 +0530

    ..

--- src/app/servicePage/page.tsx
+++ src/app/servicePage/page.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { cardData, CardData } from '@/data/cardData';
 import ServiceCard from '@/components/ui/ServiceCard';
 import ServiceHeader from '@/components/ServiceHeader';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { InsuranceData } from '@/data/insurenceData';
 import { useRouter } from 'next/navigation';
 import Buttons from '@/components/ui/buttons';
@@ -13,11 +12,9 @@ const ServiceCardPage = () => {
   const [selectedCard, setSelectedCard] = useState<string | null>(null);
   const router = useRouter();
   const { image, heading, paragraph } = InsuranceData[0];
-  const handleFirst = () => alert('First Button Clicked');
-  const handleSecond = () => alert('Second Button Clicked');
-
+  
   return (
-    <main className="min-h-screen bg-gradient-to-b from-[#e6f4fd] to-white px-4 py-8 sm:px-8 lg:px-16 flex flex-col items-center">
+    <main className="min-h-screen bg-gradient-to-b from-[#e6f4fd] to-white px-4 py-8 sm:px-8 lg:px-16 flex flex-col items-center overflow-x-hidden">
       <ServiceHeader image={image} heading={heading} paragraph={paragraph} />
 
       <div className="grid sm:grid-cols-1 md:grid-cols-1 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-8 max-w-7xl w-full mb-12">
