32997b9fd3c3c5f11f679a9a37197c9282362842
Israr8957 <ik6141333@gmail.com>
2025-07-15 11:17:26 +0530

    ..

--- src/components/cars/CarsCard.tsx
+++ src/components/cars/CarsCard.tsx
@@ -13,17 +13,22 @@ export default function CarCard({ name, icon, selected, onToggle }: CarCardProps
         {icon}
         <span>{name}</span>
       </div>
-      <input type="checkbox" checked={selected} onChange={onToggle} className="toggle toggle-primary" />
-                      <div className="ml-19 ">
-                  <label className="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" className="sr-only peer" />
-                    <div className="w-12 h-6 bg-gray-300 rounded-full peer-checked:bg-gray-500 transition-colors duration-300"></div>
-                    <div
-                      className="absolute left-0.5 top-0.5 w-5 h-5 bg-white rounded-full transition-transform duration-300
-                    peer-checked:translate-x-6"
-                    ></div>
-                  </label>
-                </div>
+      <div className="ml-4">
+      <label className="relative inline-flex items-center cursor-pointer">
+        <input
+          type="checkbox"
+          className="sr-only peer"
+          checked={selected}
+          onChange={onToggle}
+        />
+        <div className="w-12 h-6 bg-gray-300 rounded-full peer-checked:bg-gray-500 transition-colors duration-300" />
+        <div
+          className="absolute left-0.5 top-0.5 w-5 h-5 bg-white rounded-full transition-transform duration-300
+          peer-checked:translate-x-6"
+        />
+      </label>
+    </div>
+
 
     </div>
   );
