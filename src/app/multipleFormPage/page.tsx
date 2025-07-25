7fa9c7044da3110653c6ff930f696d1967e2962e
Saq65 <saklainshaikh974@gmail.com>
2025-07-25 19:20:41 +0530

    .

--- src/app/multipleFormPage/page.tsx
+++ src/app/multipleFormPage/page.tsx
@@ -132,12 +132,10 @@ export default function MultipleFormPage() {
   } else if (vinNumberConfirm && addedCarMiles === null) {
     activeHeader = MultiFormheader[4];
   } else if (addedCarMiles !== null && addedBhdValue === null) {
-    activeHeader = MultiFormheader[5]; 
+    activeHeader = MultiFormheader[5];
   } else if (addedBhdValue !== null) {
-    activeHeader = MultiFormheader[6]; 
+    activeHeader = MultiFormheader[6];
   }
-
-
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -145,222 +143,224 @@ export default function MultipleFormPage() {
 
 
   return (
-    <div className="min-h-[330vh] bg-[linear-gradient(to_bottom,_#ceedfe_0%,_white_16%,_white_70%,_#ceedfe_100%)] overflow-x-hidden scrollbar-hide">
+    <div className="min-h-[220vh] bg-[linear-gradient(to_bottom,_#ceedfe_0%,_white_16%,_white_70%,_#ceedfe_100%)] overflow-x-hidden scrollbar-hide">
       <div className='fixed  w-full'>
         <MultiformHeader />
       </div>
-      <div className="w-full max-w-7xl mx-auto px-10 mt-24">
-        <div className='flex justify-center flex-col items-center cursor-pointer mb-5 gap-10'>
-          {/* Data display blocks */}
-          {addedCars.length > 0 && (
-            <div ref={addedCarsRef} className="ml-10 space-y-2">
-              <MultiformHeading color="#8b8b8b" heading="Alright. These are the cars that I found. Which would you like to insure?" />
-              {addedCars.slice(0, 1).map((entry, index) => (
-                <div key={index} className="transition-all duration-700 transform">
-                  <div className='flex items-center gap-2'>
-                    <h3 className="text-lg font-semibold text-gray-900">
-                      {Object.values(entry).filter(Boolean).join(" ")}
-                    </h3>
-                    <BiPencil onClick={() => setShowForm(true)} className='mt-1' />
+      <div className='max-h-screen '>
+        <div className="w-full max-w-7xl mx-auto px-10 mt-24">
+          <div className='flex justify-center flex-col items-center cursor-pointer mb-5 gap-10'>
+            {addedCars.length > 0 && (
+              <div ref={addedCarsRef} className="ml-10 space-y-2">
+                <MultiformHeading color="#8b8b8b" heading="Alright. These are the cars that I found. Which would you like to insure?" />
+                {addedCars.slice(0, 1).map((entry, index) => (
+                  <div key={index} className="transition-all duration-700 transform">
+                    <div className='flex items-center gap-2'>
+                      <h3 className="text-lg font-semibold text-gray-900">
+                        {Object.values(entry).filter(Boolean).join(" ")}
+                      </h3>
+                      <BiPencil onClick={() => setShowForm(true)} className='mt-1' />
+                    </div>
                   </div>
-                </div>
-              ))}
-            </div>
-          )}
-
-          {addedDrivers.length > 0 && (
-            <div ref={addedCarsRef} className="ml-10 space-y-2">
-              <MultiformHeading color="#8b8b8b" heading="Great! Your car has been successfully added." />
-              {addedDrivers.slice(0, 1).map((entry, index) => (
-                <div key={index} className="transition-all duration-700 transform">
-                  <div className='flex items-center gap-2'>
-                    <h3 className="text-lg font-semibold text-gray-700">
-                      {Object.values(entry).filter(Boolean).join(" ")}
-                    </h3>
-                    <BiPencil className='mt-1' onClick={() => setShowDriverForm(true)} />
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
-
-          {selectedFinanceOption && (
-            <div ref={addedCarsRef} className="ml-10 space-y-2">
-              <MultiformHeading color="#8b8b8b" heading="Selected Finance Option" />
-              <div className='flex items-center gap-2'>
-                <h3 className="text-lg font-semibold text-gray-700">{selectedFinanceOption}</h3>
-                <BiPencil className='mt-1' />
-              </div>
-            </div>
-          )}
-
-          {addedVinNumber && (
-            <div ref={addedCarsRef} className="ml-10 space-y-2">
-              <MultiformHeading color="#8b8b8b" heading="Great. Is your Nissan 370Z financed or leased?" />
-              <div className='flex items-center gap-2'>
-                <h3 className="text-lg font-semibold text-gray-700">{addedVinNumber}</h3>
-                <BiPencil className='mt-1' />
-              </div>
-            </div>
-          )}
-
-          {addedCarMiles !== null && (
-            <div ref={addedCarsRef} className="ml-10 space-y-2">
-              <MultiformHeading color="#8b8b8b" heading="Estimated mileage per year" />
-              <div className='flex items-center gap-2'>
-                <h3 className="text-lg font-semibold text-gray-700">{addedCarMiles.toLocaleString()} miles/year</h3>
-                <BiPencil className='mt-1' />
-              </div>
-
-            </div>
-          )}
-
-          {addedBhdValue !== null && (
-            <div ref={addedCarsRef} className="ml-10 space-y-2">
-              <MultiformHeading color="#8b8b8b" heading="Estimated Value in BHD" />
-              <div className="flex items-center gap-2">
-                <h3 className="text-lg font-semibold text-gray-700">{addedBhdValue.toLocaleString()} BHD</h3>
-                <BiPencil className='mt-1' />
-              </div>
-
-
-            </div>
-          )}
-        </div>
-
-
-        <div className="flex flex-col xl:flex-row gap-5 w-full">
-          <aside className="w-full xl:w-1/4 hidden md:block mt-14">
-            <SidebarSteps />
-          </aside>
-
-          <main className="w-full xl:w-3/4 space-y-6 ">
-
-            <div className='flex mt-12 gap-00'>
-              <div className=''>
-                <Image
-                  src={image || "/default-avatar.png"}
-                  alt="heading image"
-                  width={55}
-                  height={55}
-                  className="rounded-full object-cover h-auto border-2 border-white shadow"
-                />
-              </div>
-              <div className={`ml-4 w-[42%]`}>
-                <MultiformHeading heading={activeHeader.heading} />
-              </div>
-            </div>
-
-            {!showForm && addedCars.length === 0 && !carConfirmed && (
-              <div className="flex flex-col gap-4 w-full sm:w-3/4 md:w-2/3 lg:w-3/5 xl:w-1/2 ml-10">
-                {cars.map((car, idx) => (
-                  <CarCard
-                    key={idx}
-                    name={car.name}
-                    selected={car.selected}
-                    onToggle={() => toggleCar(idx)}
-                  />
                 ))}
-                <AddCarCard
-                  onClick={() => setShowForm(true)}
-                  onComplete={handleCarFormComplete}
-                />
-                <NextButton
-                  disabled={!cars.some((car) => car.selected)}
-                  onClick={() => { }}
-                />
               </div>
             )}
 
-            {showForm && (
-              <div className="mt-6">
-                <CarStepForm
-                  onCancel={() => setShowForm(false)}
-                  onComplete={handleCarFormComplete}
-                />
+            {addedDrivers.length > 0 && (
+              <div ref={addedCarsRef} className="ml-10 space-y-2">
+                <MultiformHeading color="#8b8b8b" heading="Great! Your car has been successfully added." />
+                {addedDrivers.slice(0, 1).map((entry, index) => (
+                  <div key={index} className="transition-all duration-700 transform">
+                    <div className='flex items-center gap-2'>
+                      <h3 className="text-lg font-semibold text-gray-700">
+                        {Object.values(entry).filter(Boolean).join(" ")}
+                      </h3>
+                      <BiPencil className='mt-1' onClick={() => setShowDriverForm(true)} />
+                    </div>
+                  </div>
+                ))}
               </div>
             )}
 
-            {showDriverForm && !driverConfirmed && (
-              <div className="ml-10 mt-2">
-                <DriverStepForm
-                  onCancel={() => setShowDriverForm(false)}
-                  onComplete={handleDriverFormComplete}
-                />
+            {selectedFinanceOption && (
+              <div ref={addedCarsRef} className="ml-10 space-y-2">
+                <MultiformHeading color="#8b8b8b" heading="Selected Finance Option" />
+                <div className='flex items-center gap-2'>
+                  <h3 className="text-lg font-semibold text-gray-700">{selectedFinanceOption}</h3>
+                  <BiPencil className='mt-1' />
+                </div>
               </div>
             )}
 
-            {driverConfirmed && !financeConfirmed && (
-              <div className="ml-10">
-                <MultiOption data={finance} onSelect={handleOptionSelect} />
-                <NextButton
-                  disabled={!financeConfirmed}
-                  onClick={() => { }}
-                />
+            {addedVinNumber && (
+              <div ref={addedCarsRef} className="ml-10 space-y-2">
+                <MultiformHeading color="#8b8b8b" heading="Great. Is your Nissan 370Z financed or leased?" />
+                <div className='flex items-center gap-2'>
+                  <h3 className="text-lg font-semibold text-gray-700">{addedVinNumber}</h3>
+                  <BiPencil className='mt-1' />
+                </div>
               </div>
             )}
 
-            {financeConfirmed && showVinNumber && (
-              <div className="ml-10 mt-6">
-                <Vinnumber
-                  data={[]}
-                  onSelect={handleVinNumberChange}
-                  onNextClick={handleVinNumberComplete}
-                />
-                <NextButton
-                  // disabled={false}
-                  disabled={!vinnumber}
+            {addedCarMiles !== null && (
+              <div ref={addedCarsRef} className="ml-10 space-y-2">
+                <MultiformHeading color="#8b8b8b" heading="Estimated mileage per year" />
+                <div className='flex items-center gap-2'>
+                  <h3 className="text-lg font-semibold text-gray-700">{addedCarMiles.toLocaleString()} miles/year</h3>
+                  <BiPencil className='mt-1' />
+                </div>
 
-                  onClick={() => {
-                    // Add VIN number when "Next" button is clicked
-
-                    handleVinNumberComplete();
-
-                    // Scroll to the latest data (optional)
-                    if (addedCarsRef.current) {
-                      addedCarsRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
-                    }
-                  }}
-                />
               </div>
             )}
 
+            {addedBhdValue !== null && (
+              <div ref={addedCarsRef} className="ml-10 space-y-2">
+                <MultiformHeading color="#8b8b8b" heading="Estimated Value in BHD" />
+                <div className="flex items-center gap-2">
+                  <h3 className="text-lg font-semibold text-gray-700">{addedBhdValue.toLocaleString()} BHD</h3>
+                  <BiPencil className='mt-1' />
+                </div>
 
 
-            {showCarRunMiles && (
-              <div className="ml-10 mt-6">
-                <CarRunMiles
-                  max={20000}
-                  unitLabel="miles/year"
-                  defaultValue={5500}
-                  onSelect={handleCarMilesChange}
-                />
-                <NextBtn
-                  disabled={carMiles === null}
-                  onClick={handleCarMilesComplete}
-                  label="Next →"
-                />
               </div>
             )}
+          </div>
 
-            {showBHD && (
-              <div className="ml-10 mt-6">
-                <BHDComponent
-                  max={20000}
-                  unitLabel="BHD"
-                  defaultValue={5500}
-                  onSelect={handleBHDComponent}
-                />
-                <NextBtn
-                  disabled={carMiles === null}
-                  onClick={handleBHDComplete}
-                  label="Next →"
-                />
+
+          <div className="flex flex-col xl:flex-row gap-5 w-full">
+            <aside className="w-full xl:w-1/4 hidden md:block mt-14">
+              <SidebarSteps />
+            </aside>
+
+            <main className="w-full xl:w-3/4 space-y-6 ">
+
+              <div className='flex mt-12 gap-00'>
+                <div className=''>
+                  <Image
+                    src={image || "/default-avatar.png"}
+                    alt="heading image"
+                    width={55}
+                    height={55}
+                    className="rounded-full object-cover h-auto border-2 border-white shadow"
+                  />
+                </div>
+                <div className={`ml-4 w-[42%]`}>
+                  <MultiformHeading heading={activeHeader.heading} />
+                </div>
               </div>
-            )}
-          </main>
+
+              {!showForm && addedCars.length === 0 && !carConfirmed && (
+                <div className="flex flex-col gap-4 w-full sm:w-3/4 md:w-2/3 lg:w-3/5 xl:w-1/2 ml-10">
+                  {cars.map((car, idx) => (
+                    <CarCard
+                      key={idx}
+                      name={car.name}
+                      selected={car.selected}
+                      onToggle={() => toggleCar(idx)}
+                    />
+                  ))}
+                  <AddCarCard
+                    onClick={() => setShowForm(true)}
+                    onComplete={handleCarFormComplete}
+                  />
+                  <NextButton
+                    disabled={!cars.some((car) => car.selected)}
+                    onClick={() => { }}
+                  />
+                </div>
+              )}
+
+              {showForm && (
+                <div className="mt-6">
+                  <CarStepForm
+                    onCancel={() => setShowForm(false)}
+                    onComplete={handleCarFormComplete}
+                  />
+                </div>
+              )}
+
+              {showDriverForm && !driverConfirmed && (
+                <div className="ml-10 mt-2">
+                  <DriverStepForm
+                    onCancel={() => setShowDriverForm(false)}
+                    onComplete={handleDriverFormComplete}
+                  />
+                </div>
+              )}
+
+              {driverConfirmed && !financeConfirmed && (
+                <div className="ml-10">
+                  <MultiOption data={finance} onSelect={handleOptionSelect} />
+                  <NextButton
+                    disabled={!financeConfirmed}
+                    onClick={() => { }}
+                  />
+                </div>
+              )}
+
+              {financeConfirmed && showVinNumber && (
+                <div className="ml-10 mt-6">
+                  <Vinnumber
+                    data={[]}
+                    onSelect={handleVinNumberChange}
+                    onNextClick={handleVinNumberComplete}
+                  />
+                  <NextButton
+                    // disabled={false}
+                    disabled={!vinnumber}
+
+                    onClick={() => {
+                      // Add VIN number when "Next" button is clicked
+
+                      handleVinNumberComplete();
+
+                      // Scroll to the latest data (optional)
+                      if (addedCarsRef.current) {
+                        addedCarsRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+                      }
+                    }}
+                  />
+                </div>
+              )}
+
+
+
+              {showCarRunMiles && (
+                <div className="ml-10 mt-6">
+                  <CarRunMiles
+                    max={20000}
+                    unitLabel="miles/year"
+                    defaultValue={5500}
+                    onSelect={handleCarMilesChange}
+                  />
+                  <NextBtn
+                    disabled={carMiles === null}
+                    onClick={handleCarMilesComplete}
+                    label="Next →"
+                  />
+                </div>
+              )}
+
+              {showBHD && (
+                <div className="ml-10 mt-6">
+                  <BHDComponent
+                    max={20000}
+                    unitLabel="BHD"
+                    defaultValue={5500}
+                    onSelect={handleBHDComponent}
+                  />
+                  <NextBtn
+                    disabled={carMiles === null}
+                    onClick={handleBHDComplete}
+                    label="Next →"
+                  />
+                </div>
+              )}
+            </main>
+          </div>
         </div>
       </div>
+
     </div>
   );
 }
