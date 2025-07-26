577551f3a2e12f3ea54a94759f6b77bb2c21d169
Israr8957 <ik6141333@gmail.com>
2025-07-26 11:14:03 +0530

    ..

--- src/components/vinNumber/vinnumber.tsx
+++ src/components/vinNumber/vinnumber.tsx
@@ -22,13 +22,13 @@ export default function Vinnumber({ data, onSelect, onNextClick }: Props) {
   };
 
   return (
-    <div>
+    <div className='ml-7 mt-14 mb-7'>
       <div>
         <input
           type="text"
           name="vinnumber"
           id="vinnumber"
-          className="border p-3 shadow-sm rounded border-gray-400 w-2/4"
+          className="border p-4 rounded border-gray-300 w-[350px] focus:outline-none focus:border-gray-600"
           placeholder="Enter VIN Number"
           value={vinnumber}
           onChange={handleInputChange}
